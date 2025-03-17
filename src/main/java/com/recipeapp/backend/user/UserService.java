59e90c1e2e96d4a1e0c4b7197c21c98167497218
Juan Andrés Losauro <losaurojuan@gmail.com>
2025-03-17 13:42:04 -0300

    commit

---
+++ src/main/java/com/recipeapp/backend/user/UserService.java
@@ -0,0 +1,13 @@
+package com.recipeapp.backend.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+
+}
