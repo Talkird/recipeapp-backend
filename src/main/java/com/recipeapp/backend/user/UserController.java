20f3b18554a8f26b8f3102d9f8199ee18df272f2
Juan Andrés Losauro <losaurojuan@gmail.com>
2025-03-17 15:38:11 -0300

    fix

--- src/main/java/com/recipeapp/backend/user/UserController.java
+++ src/main/java/com/recipeapp/backend/user/UserController.java
@@ -1,6 +1,9 @@
 package com.recipeapp.backend.user;
 
+import java.util.List;
+  
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,11 @@ public class UserController {
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
         return userService.registerUser(user);
+    }
+
+    @GetMapping("/getUsers")
+    public List<User> getUsers() {
+        return userService.getAllUsers();
     }
 
 }
