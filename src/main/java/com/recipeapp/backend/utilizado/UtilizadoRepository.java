a485acb6712cac8f772bb32b577af912a58af287
fedemuntaabski <fmuntaabski@uade.edu.ar>
2025-05-24 16:59:52 -0300

    UtilizadoRepository

--- src/main/java/com/recipeapp/backend/utilizado/UtilizadoRepository.java
+++ src/main/java/com/recipeapp/backend/utilizado/UtilizadoRepository.java
@@ -1,7 +1,20 @@
 package com.recipeapp.backend.utilizado;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface UtilizadoRepository extends JpaRepository<Utilizado, Long> {
-    // Métodos personalizados si son necesarios
+
+    List<Utilizado> findByRecetaIdReceta(Long idReceta);
+
+
+    List<Utilizado> findByIngredienteIdIngrediente(Long idIngrediente);
+
+
+    List<Utilizado> findByUnidadIdUnidad(Long idUnidad);
+
+
+    List<Utilizado> findByObservacionesContainingIgnoreCase(String texto);
+
+
 }
